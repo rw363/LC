266f473fa72d944c53a8e0b1b689bf79cb5adb58
rw363 <rw363@cornell.edu>
2018-02-13 20:45:43 -0800

    update 15.3sum.cpp

--- 15.3sum.cpp
+++ 15.3sum.cpp
@@ -53,36 +53,40 @@
 
  */
 using namespace std;
-
+     // vector size must >= 3 or return nothing
+     // first, sort the input vector
+     // for i [0~size-1] skip the repeated number with if (i != 0 && nums[i] == nums[i-1]) continue;
+     // binary search [i+1~size-1] for nums[l] + nums[r] = -nums[i]
+     // when find a match during search, skip the repeated ones while(l < r && nums[l++] == nums[l]); and continue search
 class Solution {
 public:
      vector<vector<int>> threeSum(vector<int>& nums) {
      vector<vector<int>> ans;
      // key: sum, value: index
      if (nums.size() < 3)
-        return ans;
+          return ans;
      unordered_map<int, int>map;
      sort(nums.begin(), nums.end());
      for (int i = 0; i < nums.size(); i++)
      {
-        if (i > 0 && nums[i] == nums[i-1])
-             continue;
-        int l = i+1;
-        int r = nums.size() - 1;
-        while(l < r)
-        {
-             if (nums[l] + nums[r] == -nums[i])
-             {
-                  vector<int> holder = {nums[i], nums[l], nums[r]};
-                  ans.push_back(holder);
-                  while(l<r && nums[l++] == nums[l]);
-                  while(l<r && nums[r--] == nums[r]);
-             }
-             else if (nums[l] + nums[r] > -nums[i])
-                  r--;
-             else
-                  l++;
-        }
+          if (i > 0 && nums[i] == nums[i-1])
+               continue;
+          int l = i+1;
+          int r = nums.size() - 1;
+          while(l < r)
+          {
+               if (nums[l] + nums[r] == -nums[i])
+               {
+                    vector<int> holder = {nums[i], nums[l], nums[r]};
+                    ans.push_back(holder);
+                    while(l<r && nums[l++] == nums[l]);
+                    while(l<r && nums[r--] == nums[r]);
+               }
+               else if (nums[l] + nums[r] > -nums[i])
+                    r--;
+               else
+                    l++;
+          }
      }
      return ans;
     }

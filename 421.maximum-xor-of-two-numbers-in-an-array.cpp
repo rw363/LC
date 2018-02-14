541ef824e789eee3a0610263d7c765250b999cc7
rw363 <rw363@cornell.edu>
2018-02-14 04:46:08 -0800

    update 421.maximum-xor-of-two-numbers-in-an-array.cpp

--- 421.maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421.maximum-xor-of-two-numbers-in-an-array.cpp
@@ -56,27 +56,33 @@
 
 using namespace std;
 
+     // if A xor B = C, then A can be find by C xor B
+     // bit by bit from MSB to LSB do below (i.e. i = sizeof(int)*8 ~ 0)
+     // assume the biggest answer which is the existing ans << 1 + new bit = 1
+     // store the possible bits from the input vector into a set.    possible.insert(n>>i);
+     // check if it can be made by XOR the nums in the given array.  if (possible.find((ans|1) ^ p) != possible.end())
+
 class Solution
 {
 public:
      int findMaximumXOR(vector<int>& nums)
      {
           int ans = 0;
-          for (int i = 31; i >= 0; i--)
+          // do this bit by bit from MSB to LSB
+          for (int i = 8*sizeof(int) - 1; i >= 0; i--)
           {
+               // get ready for the next bit
                ans <<= 1;
                unordered_set<int>possible(nums.size());
+               // prepare the list
                for (auto n:nums)
-               {
                     possible.insert(n>>i);
-               }
+
+               // now try to find a match
                for (auto p:possible)
-               {
                     if (possible.find(p ^ (ans+1)) != possible.end())
-                    {
+                         // found
                          ans++;
-                    }
-               }
           }
 
           return ans;

f2bd233d4a85593c5e057f95b0f5b8545bc3f5c3
rw363 <rw363@cornell.edu>
2018-02-14 04:06:11 -0800

    update 90.subsets-ii.cpp

--- 90.subsets-ii.cpp
+++ 90.subsets-ii.cpp
@@ -67,24 +67,31 @@ using namespace std;
 
 class Solution {
 public:
-     void backtrack(vector<vector<int>> &ans, vector<int> &holder, vector<int> &nums, int start)
-     {
-          ans.push_back(holder);
-          for (int i = start; i < nums.size(); i++)
-          {
-               if (i > start && nums[i] == nums[i-1])
-                    continue;
-               holder.push_back(nums[i]);
-               backtrack(ans, holder, nums, i+1);
-               holder.pop_back();
-          }
-     }
-     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-          vector<vector<int>> ans;
-          vector<int> holder;
-          sort(nums.begin(), nums.end());
-          backtrack(ans, holder, nums, 0);
-          return ans;
-     }
-};
+    // NOTE, sort first!
+    // Then backtrack, skip if the current number to be added is the same number as the previous one IF i > start to avoid duplicate result
+    // 1) push into answer evertime since we want all combs
+    // 2) backtrack with push, start = i+1, pop
 
+    void backtrack(vector<vector<int>> &ans, vector<int> &holder, vector<int> &nums, int start)
+    {
+        // push into answer since we want all combs
+        ans.push_back(holder);
+        for (int i = start; i < nums.size(); i++)
+        {
+            // skip the duplicate result
+            if (i > start &&nums[i] == nums[i-1]) continue;
+            // else start backtrack with the next number
+            holder.push_back(nums[i]);
+            backtrack(ans, holder, nums, i + 1);
+            holder.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> holder;
+        sort(nums.begin(), nums.end());
+        backtrack(ans, holder, nums, 0);
+        return ans;
+    }
+};

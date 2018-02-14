e6e8e5a8d9079364f73301e0e80b2e6e371d12d3
rw363 <rw363@cornell.edu>
2018-02-13 20:43:30 -0800

    update 15.3sum.cpp

---
+++ 15.3sum.cpp
@@ -0,0 +1,90 @@
+/*
+
+ * [15] 3Sum
+
+
+ *
+
+ * https://leetcode.com/problems/3sum/description/
+
+ *
+
+ * algorithms
+
+ * Medium (21.82%)
+
+ * Total Accepted:    291.9K
+
+ * Total Submissions: 1.3M
+
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+
+ *
+
+ * Given an array S of n integers, are there elements a, b, c in S such that a
+
+ * + b + c = 0? Find all unique triplets in the array which gives the sum of
+
+ * zero.
+
+ * 
+
+ * Note: The solution set must not contain duplicate triplets.
+
+ * 
+
+ * 
+
+ * For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+ * 
+
+ * A solution set is:
+
+ * [
+
+ * ⁠ [-1, 0, 1],
+
+ * ⁠ [-1, -1, 2]
+
+ * ]
+
+ * 
+
+ */
+using namespace std;
+
+class Solution {
+public:
+     vector<vector<int>> threeSum(vector<int>& nums) {
+     vector<vector<int>> ans;
+     // key: sum, value: index
+     if (nums.size() < 3)
+        return ans;
+     unordered_map<int, int>map;
+     sort(nums.begin(), nums.end());
+     for (int i = 0; i < nums.size(); i++)
+     {
+        if (i > 0 && nums[i] == nums[i-1])
+             continue;
+        int l = i+1;
+        int r = nums.size() - 1;
+        while(l < r)
+        {
+             if (nums[l] + nums[r] == -nums[i])
+             {
+                  vector<int> holder = {nums[i], nums[l], nums[r]};
+                  ans.push_back(holder);
+                  while(l<r && nums[l++] == nums[l]);
+                  while(l<r && nums[r--] == nums[r]);
+             }
+             else if (nums[l] + nums[r] > -nums[i])
+                  r--;
+             else
+                  l++;
+        }
+     }
+     return ans;
+    }
+};
+

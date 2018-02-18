099c9e447200b01a07feb53dbd275e6042f7626c
rw363 <rw363@cornell.edu>
2018-02-17 19:43:35 -0800

    update 114.flatten-binary-tree-to-linked-list.cpp

--- 114.flatten-binary-tree-to-linked-list.cpp
+++ 114.flatten-binary-tree-to-linked-list.cpp
@@ -108,36 +108,47 @@
 
 using namespace std;
 
-class Solution {
-private:
-     TreeNode *prev = NULL;
-public:
+ class Solution {
+ #ifndef METHOD1
+ private:
+     TreeNode* prev = NULL;
+ #endif
+ public:
      void flatten(TreeNode* root) {
-          if (!root)
-               return;
-          flatten(root->right);
-          flatten(root->left);
-          root->left = NULL;
-          root->right = prev;
-          prev = root;
-          /*
-          if (!root)
-               return;
-          stack<TreeNode*> nodes;
-          nodes.push(root);
-          while(!nodes.empty())
-          {
-               root = nodes.top();
-               nodes.pop();
-               if (root->right)
-                    nodes.push(root->right);
-               if (root->left)
-                    nodes.push(root->left);
-               root->left = NULL;
-               if (!nodes.empty())
-                    root->right = nodes.top();
-          }
-          */
-     }
-};
+         // Note the question says flatten it to a listed list. It didn't really mean create a listed list (there is no return)
+         // Rather we need to change the tree to make it only has right child and looks like a listed list
+         // METHOD1 iterative:
+         //      1) preorder travel, instead of printing root, let root->left = NULL, root->right = nodes.top() which is the the next node,  do not pop the next node
+         //      2) check if reaches the end of the tree, either stack is empty after trying to push left/right or check nodes.empty before root->right = nodes.top()
+         // METHOD2 recursive:
+         //      1) The result seems to be preorder travel, but we do not know the next node of the linked list until we get to the next level so we need to build the tree from bottom and goes way up, like reverse preorder travel
+         //      2) Instead of root, left, right; we do right, left, root.  And we keep a global/class next pointer to tell the upper level where to point
 
+ #ifdef METHOD1
+         if (!root)
+             return;
+         stack<TreeNode*> nodes;
+         nodes.push(root);
+         while(!nodes.empty() && root)
+         {
+             root = nodes.top();
+             nodes.pop();
+             if (root->right)
+                 nodes.push(root->right);
+             if (root->left)
+                 nodes.push(root->left);
+             root->left = NULL;
+             if (!nodes.empty())
+                 root->right = nodes.top();
+         }
+ #else
+         if (!root)
+             return;
+         flatten(root->right);
+         flatten(root->left);
+         root->left = NULL;
+         root->right = prev;
+         prev = root;
+ #endif
+     }
+ };

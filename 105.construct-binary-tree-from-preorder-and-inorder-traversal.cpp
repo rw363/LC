5bdef4fc9a23649da4d6731ba9394698cfa6d307
rw363 <rw363@cornell.edu>
2018-02-18 00:37:21 -0800

    update 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -74,10 +74,18 @@
 
 using namespace std;
 
+     // 1) The first node in preoder is the root
+     // 2) The nodes before root in inorder is the left child, and the nodes after in on the right child
+     // So cut into half and divide & conqure.  TreeNode* helper(vector<int> &preorder, vector<int> inorder, int pStart, int pEnd, int iStart, int iEnd).
+     //      if pstart > pend OR istart > iend, return NULL;
+     //      left: [pstart+1 ~ pend],                   [istart ~ i-1]
+     //      right: [pstart+(i+1-istart)  ~ pend]       [i+1 ~ iend]
+
 class Solution {
 public:
      TreeNode* helper(vector<int> &preorder, int pStart, vector<int> &inorder, int iStart, int iEnd)
      {
+          // first find the index of root in inorder ;
           if (iStart > iEnd)
                return NULL;
           TreeNode* root = new TreeNode(preorder[pStart]);
@@ -87,6 +95,7 @@ public:
                if (inorder[i] == preorder[pStart])
                     break;
           }
+          // now cut them into half and connect both left and right child to root
           root->left = helper(preorder, pStart+1, inorder, iStart, i-1);
           root->right = helper(preorder, pStart+1+(i-iStart), inorder, i+1, iEnd);
           return root;

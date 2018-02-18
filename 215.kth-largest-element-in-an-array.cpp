5226f4a40d161e73394c45b585b2d051b6e1d1da
rw363 <rw363@cornell.edu>
2018-02-17 21:12:35 -0800

    update 215.kth-largest-element-in-an-array.cpp

--- 215.kth-largest-element-in-an-array.cpp
+++ 215.kth-largest-element-in-an-array.cpp
@@ -50,39 +50,60 @@ using namespace std;
 
 class Solution {
 public:
-     int partition (vector<int> &nums, int start, int end)
-     {
-          swap(nums[end], nums[start + rand()%(end-start+1)]);
-          int p = nums[end];
-          int w_i = start;
-          for (int i = start; i < end; i++)
-          {
-               if (nums[i] > p)
-               {
-                    swap(nums[i], nums[w_i++]);
-               }
-          }
-          swap(nums[end], nums[w_i]);
-          return w_i;
-     }
+    // METHOD1: sort it and find the kth largest
+    // METHOD2:
+    //          1) modifiy the quick sort and only sort the part that contains the kth largest number. To find the kth eaiser, sort in decreasing order.   if (nums[i] > pivot) swap(nums[i], nums[w_i++]);
+    //          2) instead of calling quicksort recursively if (start < end), we only partition the part contains kth node with while (start < end)
+    //                  a) Since p_i is 0 based, there is p_i + 1 numbers less than the number at p_i:  if (p_i + 1 == k) return nums[p_i];
+    //                  b) if we do not have enough small numbers, the target number is at the bigger part.  Since the value of p_i is a abosulte value, no need offset      else if (p_i + 1 < k)  start = p_i + 1;
+    //                  c) else     end = p_i - 1;
+    //          3) if we finished the while loop before finding the target, return -1
 
-     int findKthLargest(vector<int>& nums, int k) {
-          int n = nums.size();
-          if (n < k)
-               return -1;
-          int start = 0;
-          int end = n - 1;
-          while(start <= end)
-          {
-               int p_i = partition(nums, start, end);
-               if (p_i+1 == k)
-                    return nums[p_i];
-               else if (p_i+1 > k)
-                    end = p_i - 1;
-               else
-                    start = p_i + 1;
-          }
-          return -1;
-     }
+    int partition(vector<int> &nums, int start, int end)
+    {
+        int rpi = start + rand()%(end-start+1);
+        int pivot = nums[rpi];
+        swap(nums[rpi], nums[end]);
+
+        int w_i = start;
+        for (int i = start; i < end; i++)
+        {
+            if (nums[i] > pivot)
+                swap(nums[i], nums[w_i++]);
+        }
+        swap(nums[w_i], nums[end]);
+        return w_i;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        int start = 0;
+        int end = nums.size() - 1;
+        while(start <= end)
+        {
+            int p_i = partition(nums, start, end);
+            // since p_i is 0 based, there is p_i + 1 numbers less than the number at p_i
+            if (p_i + 1 == k)
+                return nums[p_i];
+            // if we do not have enough small numbers, the target number is at the bigger part
+            // since the value of p_i is a abosulte value, no need offset
+            else if (p_i + 1 < k)
+                start = p_i + 1;
+            else
+                end = p_i - 1;
+        }
+        // if reaches here, we can not find it
+        return -1;
+    }
+
+    /*
+    void quicksort(vector<int> &num, int start, int end)
+    {
+        if (start < end)
+        {
+            int p_i = parition(nums, start, end);
+            quicksort(nums, start, p_i - 1);
+            quicksort(nums, p_i + 1, end);
+        }
+    }
+    */
 };
-
